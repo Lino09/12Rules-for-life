398172bf277b0350c3d096c41bd5b69165d6a0e8
Lino09 <onosmx@gmail.com>
2021-10-12 19:13:44 -0500

    Improve image position and text in speakers section

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,37 @@
 const speakers = [
   {
     name: 'Dr. Jordan B. Peterson',
-    bio: 'Dr. Jordan B. Peterson is a clinical psychologist, and the author of the bestsellers Beyond Order: 12 More Rules for Life & 12 Rules for Life: An Antidote to Chaos.',
+    bio: 'Dr. Jordan B. Peterson is a clinical psychologist, and the author of the bestseller 12 Rules for Life: An Antidote to Chaos.',
     occupation: 'Professor of psychology at the University of Toronto',
     img: '../assets/png/speakers/peterson.png',
   },
   {
     name: 'Dr. Carl Gustav Jung',
-    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id enim quis ex suscipit iaculis. Fusce nibh velit, bibendum et.',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id enim quis.',
     occupation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     img: '../assets/png/speakers/jung.png',
   },
   {
     name: 'Wilhelm Wundt',
-    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id enim quis ex suscipit iaculis. Fusce nibh velit, bibendum et.',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id enim quis.',
     occupation: 'Father of modern psycology',
     img: '../assets/png/speakers/wilhelm.png',
   },
   {
     name: 'Dr. Frederick B. Skinner',
-    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id enim quis ex suscipit iaculis. Fusce nibh velit, bibendum et.',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id enim quis.',
     occupation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     img: '../assets/png/speakers/skinner.png',
   },
   {
     name: 'Dr. Abraham Maslow',
-    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id enim quis ex suscipit iaculis. Fusce nibh velit, bibendum et.',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id enim quis.',
     occupation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     img: '../assets/png/speakers/maslow.png',
   },
   {
     name: 'Dr. Sigmund Freud',
-    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id enim quis ex suscipit iaculis. Fusce nibh velit, bibendum et.',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id enim quis.',
     occupation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     img: '../assets/png/speakers/freud.png',
   },
